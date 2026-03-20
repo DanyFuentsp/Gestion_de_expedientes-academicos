5f22cbba4f8584b6588abcb5e9899be8bdc808d6
DanyFuentsp <fp230748@alumno.udb.edu.sv>
2026-03-19 23:00:19 -0600

    prueba de git

--- Gestion_de_expedientes academicos/Program.cs
+++ Gestion_de_expedientes academicos/Program.cs
@@ -1,3 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("     //==============================================//");
+        Console.WriteLine("    //        EXAMEN DE ESTRUCTURAS DE DATOS        //");
+        Console.WriteLine("//===========================================================//");
+        Console.WriteLine("     Bienvenido al sistema de gestión de expedientes académicos.");
+        Console.WriteLine("         Por favor, registre el expediente académico:");
+        Console.WriteLine("//===========================================================//");
+
+        Console.Write("Ingrese el nombre completo: ");
+        string nombreCompleto = Console.ReadLine();
+
+        Console.Write("Ingrese la carrera a estudiar: ");
+        string carrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el carnet del estudiante: ");
+        int carnet = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la fecha de nacimiento (dd/mm/yyyy): ");
+        bool fechaValida = DateTime.TryParse(Console.ReadLine(), out DateTime fechaNacimiento);
 
 
+    }
+}
